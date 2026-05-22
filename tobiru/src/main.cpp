01d7bfe405e397cd2658c4f486e8e6f7963928ba
Rania Maaraba <rhaas23@outlook.com>
2026-05-22 17:29:44 -0400

    Slate for multithreading functions
    Loop may need to be handled manually

--- tobiru/src/main.cpp
+++ tobiru/src/main.cpp
@@ -1,9 +1,21 @@
+/**
+05/22/2026 - RH - Basework for multithreading integration
+*/
+
 #include <Arduino.h>
+#include <Adafruit_LSM6DSO32.h>
+#include <MS5611.h>
 // Note that the GPIO number is offset by one so D2 is actually GPIO3 instead of GPIO2
 
-#define LED1_PIN 3 // 2 + 1
-#define LED2_PIN 5 // 4 + 1
+// #define LED1_PIN 3 // 2 + 1
+// #define LED2_PIN 5 // 4 + 1
 
+//Sensors
+// Adafruit_LSM6DSO32 dso32;
+// MS5611 baro(0x77);
+
+
+//Tasks
 TaskHandle_t Task1;
 TaskHandle_t Task2;
 
@@ -11,56 +23,115 @@ const int led1 = 3;
 const int led2 = 5; //possibly may need to change
 
 //Code section for tasks
-void Task1code(void *pvParameters)  {
-  Serial.print("Task1 running on core ");
-  Serial.println(xPortGetCoreID());
+void startUp(void *pvParameters){
 
-  for(;;) { //blink for 1 second
-    digitalWrite(led1, HIGH);
-    delay(1000);
-    digitalWrite(led1, LOW);
-    delay(1000);
-  }
 }
 
-void Task2code(void *pvParameters){
-  Serial.print("Task2 runnin on core ");
-  Serial.println(xPortGetCoreID());
+void imuWrite(void *pvParameters)  {
 
-  for(;;) { //blink every 0.5 s
-    digitalWrite(led2, HIGH);
-    delay(700);
-    digitalWrite(led2, LOW );
-    delay(700);
-  }
+}
+
+void readBarometer(void *pvParameters){
+
+}
+
+void writeEvents(void *pvParameters){
+
+}
+
+void fileLogging(void *pvParameters){
+
+}
+
+void shutDown(void *pvParameters){
+
+}
+
+void userTests(void *pvParameters){
+
 }
 
 void setup() {
   Serial.begin(115200); //default baud rate
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
+  delay(500);
 
-  //task 1 to core 0
+  /*
+  Core 0:
+    Used for all event logging
+      Startup and Shutdown
+      File logging
+      Events
+      User tests
+  Core 1:
+    All fetching from instruments
+      IMU
+      Accel/Baro
+  */
+
+  //Core 0 Tasks
   xTaskCreatePinnedToCore(
-    Task1code, 
-    "Task1",   
-    10000,      
+    startUp, 
+    "Starting",   
+    10000,     //stack size, will want to change later into development 
     NULL,      
-    1,         
-    &Task1,     
+    1,      //additionally may want to consider changing priorites   
+    &startUp,     
     0);                   
-  delay(500); 
 
-  //task 2 to core 1
   xTaskCreatePinnedToCore(
-    Task2code,   
-    "Task2",   
+    writeEvents,   
+    "eventLogging",   
     10000,     
     NULL,      
     1,          
-    &Task2,   
+    &writeEvents,   
     1);        
-    delay(500); 
+
+  xTaskCreatePinnedToCore(
+    fileLogging,   
+    "fileLogging",   
+    10000,     
+    NULL,      
+    1,          
+    &TasfileLoggingk2,   
+    1);        
+
+  xTaskCreatePinnedToCore(
+    userTests,   
+    "TestMode",   
+    10000,     
+    NULL,      
+    1,          
+    &userTests,   
+    1);        
+
+  xTaskCreatePinnedToCore(
+    shutDown,   
+    "PoweringOff",   
+    10000,     
+    NULL,      
+    1,          
+    &shutDown,   
+    1);        
+
+  //Core 1 Tasks
+  xTaskCreatePinnedToCore(
+    imuWrite,   
+    "imu",   
+    10000,     
+    NULL,      
+    1,          
+    &imuWrite,   
+    1);        
+
+    xTaskCreatePinnedToCore(
+    readBarometer,   
+    "barometer",   
+    10000,     
+    NULL,      
+    1,          
+    &readBarometer,   
+    1);        
 }
 
 
